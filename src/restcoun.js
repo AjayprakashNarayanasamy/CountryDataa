c9792d16aaad44780b47727cfd4efb9a0eeb2ca0
AjayprakashNarayanasamy <87938418+AjayprakashNarayanasamy@users.noreply.github.com>
2022-03-01 22:14:07 +0530

    Update restcoun.js

--- src/restcoun.js
+++ src/restcoun.js
@@ -1,82 +1,80 @@
-import React ,{useState , useEffect}from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-function Reactrescoun()
-{
+function Reactrescoun() {
 
-    const[restcountry , setrestcountry] = useState([]);
+   const [restcountry, setrestcountry] = useState([]);
 
- useEffect( async function value()
- {
-   var response = await axios.get("http://api.countrylayer.com/v2/all?access_key=5ba67013f271cf3f0214bf207ea69ce9");
-   console.log(response)
-   setrestcountry(response.data)
-   console.log(restcountry)
-   
- 
- })
+   useEffect(async function value() {
+      var response = await axios.get("http://api.countrylayer.com/v2/all?access_key=ae04e8407ed29a4146632ab5dcb32c92");
 
-
-
- 
-
-  
-    
-
-    return(
-        <div >
-           <h3 className="div1">Country Data</h3>
-
-{ <table className="div2" >
-   <thead>
-      <tr>
-      <td className="td1"><b>Country</b></td>
-        <td className="td1"><b>Capital</b></td>
-        <td className="td1"><b>Continent</b> </td>
-        <td className="td1"><b>Alpha Code</b></td>
-
-      </tr>
-
-       </thead>
+      setrestcountry(response.data)
       
-<tbody>
-
-{restcountry.map((data)=>(
-<tr key = {data.name}>
-
-<td>
-{data.name}
-
-</td>
-<td>
-{data.capital}
-
-</td>
-<td>
-{data.region}
-</td>
-<td>
-{data.alpha3Code}
-</td>
 
 
+   },[])
 
-</tr>
-)
-)}
+console.log(restcountry)
+   
 
 
 
 
 
+   return (
+      <div >
+         <h3 className="div1">Country Data</h3>
 
-       </tbody>
+         {<table className="div2" >
+            <thead>
+               <tr>
+                  <td className="td1"><b>Country</b></td>
+                  <td className="td1"><b>Capital</b></td>
+                  <td className="td1"><b>Continent</b> </td>
+                  <td className="td1"><b>Alpha Code</b></td>
 
-    </table>
+               </tr>
+
+            </thead>
+
+            <tbody>
+
+               {restcountry.map((data) => (
+                  <tr key={data.name}>
+
+                     <td>
+                        {data.name}
+
+                     </td>
+                     <td>
+                        {data.capital}
+
+                     </td>
+                     <td>
+                        {data.region}
+                     </td>
+                     <td>
+                        {data.alpha3Code}
+                     </td>
 
 
- }
 
-        </div>
-    )
+                  </tr>
+               )
+               )}
+
+
+
+
+
+
+            </tbody>
+
+         </table>
+
+
+         }
+
+      </div>
+   )
 }
 export default Reactrescoun;
